4937ca275c40f1aded7cbd598fcc938f6d3d969a
AudeizReading <aaericia@hotmail.fr>
2021-08-12 11:28:55 +0200

    Upgrading error handling with a structure and a function looping on a tab of these structs, taking in count about error that is errno and custom errors

--- srcs/main.c
+++ srcs/main.c
@@ -1,10 +1,48 @@
 #include "../includes/so_long.h"
 
+typedef struct	s_error
+{
+	int		num;
+	char	*message;
+	t_bool	must_exit;
+}				t_error;
+
 void	ft_puterror(void)
 {
 	ft_putendl_fd("Error", 2);
-	ft_putendl_fd(strerror(errno), 2);
-	exit(EXIT_FAILURE);
+}
+
+void	ft_puterr(char *err, t_bool must_exit)
+{
+	ft_puterror();
+	ft_putendl_fd(err, 2);
+	if (must_exit)
+		exit(EXIT_FAILURE);
+}
+
+void	ft_parse_map_error(int errnum)
+{
+	int		i;
+	static t_error	tab_err[] = {
+		{1024, "Error encountered while reading map's lines", e_true},
+		{1025, "Not a .ber map", e_true},
+		{1026, "Map is not rectangular", e_true},
+		{1027, "Map is not entirely closed by walls", e_true},
+		{1028, "Map contains invalid characters", e_true},
+		{1029, "Map does not have any exit", e_true},
+		{1030, "Map does not have any collectible", e_true},
+		{1031, "Map does not have any starting position", e_true},
+		{-1, NULL, e_false}
+	};
+
+	i = -1;
+	while (tab_err[i++].num != -1)
+	{
+		if (tab_err[i].num == errnum)
+			ft_puterr(tab_err[i].message, tab_err[i].must_exit);
+		else
+			ft_puterr(strerror(errno), e_true);
+	}
 }
 
 int		main(void)
@@ -15,12 +53,10 @@ int		main(void)
 
 	gnl = 1;
 	line = NULL;
-	fd = open("srcs/main.c", O_RDONLY);
-	//fd = open("main.c", O_RDONLY);
+	//fd = open("srcs/main.c", O_RDONLY);
+	fd = open("main.c", O_RDONLY);
 	if (fd == -1)
-	{
-		ft_puterror();
-	}
+		ft_parse_map_error(errno);
 	while (gnl > 0)
 	{
 		gnl = get_next_line(fd, &line);
@@ -28,10 +64,7 @@ int		main(void)
 		free(line);
 	}
 	if (gnl == -1)
-	{
-		errno = 5;
-		ft_puterror();
-	}
+		ft_parse_map_error(1024);
 	close(fd);
 	return (0);
 }
