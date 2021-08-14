48ad595dc7bad6d64ef32522e9896070331d6564
AudeizReading <aaericia@hotmail.fr>
2021-08-14 15:52:21 +0200

    Working on my own linked list because need more informations about map line, create functions about handle it

--- srcs/main.c
+++ srcs/main.c
@@ -2,9 +2,18 @@
 
 typedef struct	s_point
 {
-	int		x;
-	int		y;
+	int				x;
+	int				y;
+	struct s_point	*next;
 }				t_point;
+
+typedef struct	s_line
+{
+	char			*content;
+	int				y;
+	size_t			len;
+	struct	s_line	*next;
+}				t_line;
 
 typedef struct	s_map
 {
@@ -16,18 +25,101 @@ typedef struct	s_map
 //	t_list	*line;
 }				t_map;
 
+t_line	*ft_init_line(char *content, int pos)
+{
+	t_line	*new_line;
+
+	new_line = (t_line *)malloc(sizeof(*new_line));
+	if (!new_line)
+		return (NULL);
+	new_line->content = ft_strdup(content);
+	new_line->y = pos;
+	new_line->len = ft_strlen(new_line->content);
+	new_line->next = NULL;
+	return (new_line);
+}
+
+int			ft_map_size(t_line *map)
+{
+	int			size;
+
+	size = 0;
+	while (map)
+	{
+		size++;
+		map = map->next;
+	}
+	return (size);
+}
+
+t_line	*ft_last_line(t_line *map)
+{
+	if (!map)
+		return (NULL);
+	while (map->next)
+		map = map->next;
+	return (map);
+}
+
+void		ft_map_addback(t_line **map, t_line *line)
+{
+	t_line	*last_line;
+
+	if (!*map)
+	{
+		(*map) = line;
+		return ;
+	}
+	last_line = ft_last_line(*map);
+	last_line->next = line;
+}
+
+void		ft_map_delone(t_line *line, void (*del)(void *))
+{
+	(*del)(line->content);
+	free(line);
+	line = NULL;
+}
+
+void		ft_map_clear(t_line **map, void (*del)(void *))
+{
+	t_line	*tmp;
+
+	while (*map)
+	{
+		tmp = (*map)->next;
+		ft_map_delone(*map, del);
+		(*map) = tmp;
+	}
+}
+
+void		ft_print_map(t_line *map)
+{
+	while (map)
+	{
+		ft_putendl_fd(map->content, 1);
+		ft_putnbr_fd(map->y, 1);
+		ft_putstr_fd(" pos y\n", 1);
+		ft_putnbr_fd(map->len, 1);
+		ft_putstr_fd(" length line\n", 1);
+		map = map->next;
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	int		fd;
 	int		gnl;
 	char	*line;
 	t_map	*map;
-	t_list	*map_line;
-	t_list	*tmp;
+	t_line	*m_line;
+	t_line	*m_tmp;
+	int		y;
 
 	gnl = 1;
+	y = 0;
 	line = NULL;
-	map_line = NULL;
+	m_line = NULL;
 	map  = &(t_map){0, 0, /*{0, 0}, {0, 0}*/};
 	if (argc == 2)
 	{
@@ -36,27 +128,32 @@ int		main(int argc, char **argv)
 			ft_parse_map_error(errno);
 		if (!ft_is_map_file(argv[1], ".ber"))
 			ft_parse_map_error(79);
-		ft_putstr_fd("\n", 1);
 		while (gnl > 0)
 		{
 			gnl = get_next_line(fd, &line);
-			// Here check if map is rectangular or have 0, 1, C, E and P chars
-			// Check if it is wall all around"
-			ft_putendl_fd(line, 1);
-			tmp = ft_new_mapline(line);
+		//	ft_putendl_fd(line, 1);
 			if (gnl)
-				ft_lstadd_back(&map_line, tmp);/**/
+			{
+				m_tmp = ft_init_line(line, y++);
+				//y++;
+				ft_map_addback(&m_line, m_tmp);
+			}
 			free(line);
 		}
 		if (gnl == -1)
 			ft_parse_map_error(errno);
-		ft_putendl_fd("------MAP------------------------", 1);
+	/*	ft_putendl_fd("------MAP------------------------", 1);
 		ft_print_list(map_line);
 		ft_putendl_fd("---------------------------------", 1);
 		ft_putnbr_fd(ft_lstsize(map_line), 1);
+		ft_putendl_fd(" maillons", 1);*/
+		ft_putendl_fd("------MY_MAP---------------------", 1);
+		ft_print_map(m_line);
+		ft_putendl_fd("---------------------------------", 1);
+		ft_putnbr_fd(ft_map_size(m_line), 1);
 		ft_putendl_fd(" maillons", 1);
-		ft_lstclear(&tmp, free);
-		ft_lstclear(&map_line, free);
+//		ft_lstclear(&map_line, free);
+		ft_map_clear(&m_line, free);
 		close(fd);
 	}
 	else
