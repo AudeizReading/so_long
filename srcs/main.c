e040ef02f780d661aaabd8a3b06ed2c214f96abb
AudeizReading <aaericia@hotmail.fr>
2021-08-15 22:35:52 +0200

    Getting all the starting, collectibles and exit position, have to refactoring the whole but I have done a great job I have never done about, so I deserve some rest for now

--- srcs/main.c
+++ srcs/main.c
@@ -4,9 +4,9 @@ typedef struct	s_map
 {
 	size_t	cols;
 	size_t	lines;
-//	t_point	*start;
-//	t_point	*collect;
-//	t_point	*end;
+	t_point	*start;
+	t_point	*collect;
+	t_point	*end;
 	t_line	*first_line;
 }				t_map;
 
@@ -24,7 +24,7 @@ void	print_info_map(t_map *map)
 	ft_putendl_fd("---------------------------------", 1);
 }
 
-void	ft_check_valid_characters(t_line *map)
+t_bool	ft_check_valid_characters(t_line *map)
 {
 	char	*p_content;
 
@@ -34,13 +34,41 @@ void	ft_check_valid_characters(t_line *map)
 		while (*p_content)
 		{
 			if (!ft_strchr("01CEP", *p_content))
-				ft_parse_map_error(1029);
-			/*else
-				ft_putchar_fd(*p_content, 1);*/
+				return (e_false);
 			p_content++;
 		}
 		map = map->next;
 	}
+	return (e_true);
+}
+
+t_point	*ft_has_object(t_line *line, char c, int i)
+{
+	char	*p_content;
+	t_point	*point;
+	t_point	*list;
+	int		x;
+
+	list = NULL;
+	while (line)
+	{
+		p_content = line->content;
+		x = 0;
+		while (*p_content)
+		{
+			if (*p_content == c)
+			{
+				point = ft_init_point(x, line->y, i++);
+				if (!point)
+					return (NULL);
+				ft_point_addback(&list, point);
+			}
+			p_content++;
+			x++;
+		}
+		line = line->next;
+	}
+	return (list);
 }
 
 t_bool	ft_is_rectangular_map(t_line *map)
@@ -51,7 +79,7 @@ t_bool	ft_is_rectangular_map(t_line *map)
 	while (map)
 	{
 		if (first_line_len != map->len)
-			ft_parse_map_error(1027);
+			return (e_false);
 		map = map->next;
 	}
 	return (e_true);
@@ -62,7 +90,7 @@ int		main(int argc, char **argv)
 	int		fd;
 	t_map	*map;
 
-	map  = &(t_map){0, 0, NULL};
+	map  = &(t_map){0, 0, NULL, NULL, NULL, NULL};
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
@@ -76,24 +104,28 @@ int		main(int argc, char **argv)
 		map->lines = ft_map_size(map->first_line);
 		map->cols = map->first_line->len;
 		print_info_map(map);
-		/* Test liste chainee struct point */
-		/*t_point	*start_test = NULL;
-		t_point	*point_test;
-		int		i = 0;
-
-		while (i < 3)
-		{
-			point_test = ft_init_point(8, 3, i);
-			ft_point_addback(&start_test, point_test);
-			ft_putnbr_fd(i, 1);
-			ft_putendl_fd(" i", 1);
-			i++;
-		}
-		ft_print_point_list(start_test);
-		ft_point_clear(&start_test, free);*/
-		ft_check_valid_characters(map->first_line);
-		if (ft_is_rectangular_map(map->first_line))
-			ft_putendl_fd("Map is rectangular", 1);
+		if(!ft_check_valid_characters(map->first_line))
+			ft_parse_map_error(1029);
+		if (!ft_is_rectangular_map(map->first_line))
+			ft_parse_map_error(1027);
+		map->start = ft_has_object(map->first_line, 'P', 0);
+		if (!map->start)
+			ft_parse_map_error(1032);
+		ft_putendl_fd("Liste des points de depart", 1);
+		ft_print_point_list(map->start);
+		ft_point_clear(&map->start, free);
+		map->collect = ft_has_object(map->first_line, 'C', 0);
+		if (!map->collect)
+			ft_parse_map_error(1031);
+		ft_putendl_fd("Liste des points de collectibles", 1);
+		ft_print_point_list(map->collect);
+		ft_point_clear(&map->collect, free);
+		map->end = ft_has_object(map->first_line, 'E', 0);
+		if (!map->end)
+			ft_parse_map_error(1030);
+		ft_putendl_fd("Liste des points de sortie", 1);
+		ft_print_point_list(map->end);
+		ft_point_clear(&map->end, free);
 		ft_map_clear(&map->first_line, free);
 		close(fd);
 	}
